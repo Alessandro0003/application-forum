ac1a5c6ee508a177ded4797a2e23713a94fd0fff
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-05 13:14:23 -0300

    :recycle: refactor: settings eslint

--- tests/use-cases/answer-question.spec.ts
+++ tests/use-cases/answer-question.spec.ts
@@ -2,20 +2,18 @@ import { AnswerQuestionUseCase } from '@/domain/use-cases/answer-question'
 import { AnswersRepository } from '@/domain/repositories/answers-repository'
 import { Answer } from '@/domain/entities/answer'
 
-
 const fakeAnswersRepository: AnswersRepository = {
-    create: async (answer: Answer) => {
-        return
-    }
+  create: async (answer: Answer) => {
+  }
 }
 test('create an answer', async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-    const answer = await answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'Nova resposta',
-    })
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta'
+  })
 
-    expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('Nova resposta')
 })
