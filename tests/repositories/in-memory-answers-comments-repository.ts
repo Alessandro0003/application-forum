dca2bbf7b5ca4cb57f7ff977382c82bf73fd1ec9
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-10 19:25:59 -0300

    :sparkles: feat: create functionality FeatchAnswerComment

--- tests/repositories/in-memory-answers-comments-repository.ts
+++ tests/repositories/in-memory-answers-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersCommentsRepository } from '@/domain/forum/application/repositories/answers-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -12,6 +13,14 @@ export class InMemoryAnswersCommentsRepository implements AnswersCommentsReposit
     }
 
     return answerComment
+  }
+
+  async findManyByAnswerId (answerId: string, { page }: PaginationParams): Promise<AnswerComment[]> {
+    const answersComment = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answersComment
   }
 
   async create (AnswerComment: AnswerComment) {
