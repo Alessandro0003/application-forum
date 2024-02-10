012030a08970692e0211db0ec16ee09fb1a3d353
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-10 01:12:54 -0300

    :sparkles: feat: create functionality FetchRecentTopicsUseCase

---
+++ src/domain/forum/application/use-cases/fetch-recent-topics.ts
@@ -0,0 +1,24 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionRepository } from '../repositories/question-repository'
+
+interface FetchRecentTopicsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentTopicsUseCaseResponse {
+  question: Question[]
+}
+
+export class FetchRecentTopicsUseCase {
+  constructor (
+    private readonly questionRepository: QuestionRepository
+  ) {}
+
+  async execute ({ page }: FetchRecentTopicsUseCaseRequest): Promise<FetchRecentTopicsUseCaseResponse> {
+    const question = await this.questionRepository.findManyRecent({ page })
+
+    return {
+      question
+    }
+  }
+}
