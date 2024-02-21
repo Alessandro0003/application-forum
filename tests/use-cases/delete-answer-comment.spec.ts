9c04054c08d9d426c81e1b9946d775cc93bf4b44
Alessandro0003 <alessandroquinta3@gmail.com>
2024-02-21 01:07:18 -0300

    :test_tube: test: refactor test to return is method function isLeft to response error

--- tests/use-cases/delete-answer-comment.spec.ts
+++ tests/use-cases/delete-answer-comment.spec.ts
@@ -1,16 +1,17 @@
-import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
 import { InMemoryAnswersCommentsRepository } from '../repositories/in-memory-answers-comments-repository'
 import { makeAnswerComment } from 'tests/factories/make-answer-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/domain/forum/application/use-cases/errors/not-allowed-error'
+import { DeleteCommentAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
 
 let inMemoryAnswersCommentsRepository: InMemoryAnswersCommentsRepository
-let sut: DeleteAnswerUseCase
+let sut: DeleteCommentAnswerUseCase
 
 describe('Delete Answer Comment', () => {
   beforeEach(() => {
     inMemoryAnswersCommentsRepository = new InMemoryAnswersCommentsRepository()
 
-    sut = new DeleteAnswerUseCase(inMemoryAnswersCommentsRepository)
+    sut = new DeleteCommentAnswerUseCase(inMemoryAnswersCommentsRepository)
   })
 
   it('should be able to delete answer comment ', async () => {
@@ -20,7 +21,7 @@ describe('Delete Answer Comment', () => {
 
     await sut.execute({
       authorId: answerComment.authorId.toString(),
-      answerId: answerComment.id.toString()
+      answerCommentId: answerComment.id.toString()
     })
 
     expect(inMemoryAnswersCommentsRepository.items).toHaveLength(0)
@@ -33,11 +34,12 @@ describe('Delete Answer Comment', () => {
 
     await inMemoryAnswersCommentsRepository.create(answerComment)
 
-    expect(async () => {
-      return await sut.execute({
-        authorId: answerComment.authorId.toString(),
-        answerId: 'author-2'
-      })
-    }).rejects.toBeInstanceOf(Error)
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: 'author-2'
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
