06a94085dff0c4dd862a9269def34f4930ad6404
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-04 13:45:51 -0300

    🚀 feat: add attachmentsId to edit answer

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -3,21 +3,27 @@ import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerAttachmentsList } from '../../enterprise/entities/answer-attachment-list'
+import { AnswerAttachmentsRepository } from '../repositories/answers-attachment-repository'
 
 interface EditAnswerUseCaseRequest {
   authorId: string
   answerId: string
   content: string
+  attachmentsIds: string[]
 }
 
 type EditAnswerUseCaseResponse = Either <ResourceNotFoundError | NotAllowedError, { answer: Answer }>
 
 export class EditAnswerUseCase {
   constructor (
-    private readonly answerRepository: AnswersRepository
+    private readonly answerRepository: AnswersRepository,
+    private readonly answersAttachmentsRepository: AnswerAttachmentsRepository
   ) {}
 
-  async execute ({ authorId, answerId, content }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+  async execute ({ authorId, answerId, content, attachmentsIds }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
     const answer = await this.answerRepository.findById(answerId)
 
     if (!answer) {
@@ -27,6 +33,21 @@ export class EditAnswerUseCase {
     if (authorId !== answer.authorId.toString()) {
       return left(new NotAllowedError())
     }
+
+    const currentAnswerAttachments = await this.answersAttachmentsRepository.findManyByAnswerId(answerId)
+
+    const answerAttachmentList = new AnswerAttachmentsList(currentAnswerAttachments)
+
+    const answerAttachments = attachmentsIds.map((attachmentId) => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        answerId: answer.id
+      })
+    })
+
+    answerAttachmentList.update(answerAttachments)
+
+    answer.attachments = answerAttachmentList
 
     answer.content = content
 
