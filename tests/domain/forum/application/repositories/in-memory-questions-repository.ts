0d0a05c795c2c20b113398510133b4f4afe52e2d
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-13 16:56:11 -0300

    🚀 feat: add declaration DomainEvents to InMemoryQuestionsRepository in functions save and create

--- tests/domain/forum/application/repositories/in-memory-questions-repository.ts
+++ tests/domain/forum/application/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachment-repository'
 import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
@@ -40,10 +41,14 @@ export class InMemoryQuestionRepository implements QuestionRepository {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items[itemIndex] = question
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async create (question: Question) {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete (question: Question) {
